83b01a6fd191f31e2c8dca74dabf0372acf4df78
William Espindola <oi@williamespindola.com.br>
2019-11-16 01:07:02 -0300

    gh-pages

---
+++ precache-manifest.7eb9e0cf0c722eac4f7e559a8349c337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86cc27f243a0a139bb486f2c3bbe1f51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b146db36b9d4321be42",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "3c55f1bf0a262cf9df9c",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "0b146db36b9d4321be42",
+    "url": "/static/js/2.69f37bf8.chunk.js"
+  },
+  {
+    "revision": "3c55f1bf0a262cf9df9c",
+    "url": "/static/js/main.ba2159a3.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
